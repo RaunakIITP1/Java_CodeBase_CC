28bffb801a358bb1e5c4ac2949a989e99f8a8723
Raunak Kumar <raunak.iitp@zohomail.in>
2026-02-04 05:45:26 +0530

    initial

---
+++ src/Day1/ArrayBasics.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class ArrayBasics {
+    static void main(String[] args) {
+//        int x=98;
+
+        int [] arr = {20, 24, 26, 27, 28};
+
+//        System.out.println(arr[0]);
+
+
+//        Traverse the array
+
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
